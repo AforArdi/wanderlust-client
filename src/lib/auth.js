0e3c5100348ea3cec82062b255b54bdce01eeaa8
AforArdi <binsbro36@gmail.com>
2026-05-16 22:42:40 +0600

    configured auth and auth-client to generate jwt token

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db('wanderlust_db');
@@ -10,13 +11,24 @@ export const auth = betterAuth({
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
-  emailAndPassword: { 
-    enabled: true, 
+  // setup
+  session: {
+    cookieCache: {
+      enabled: true,
+      strategy: 'jwt',
+      maxAge: 2 * 60 * 60 * 24, // 2 day
+    }
+  },
+  plugins: [
+    jwt(),
+  ],
+  emailAndPassword: {
+    enabled: true,
   },
   socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }, 
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
+  },
 });
